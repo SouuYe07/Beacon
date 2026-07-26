7b4a07f3f453670e161a127c7e458e90c0598fa6
SouuYe07 <iesouaguila07@gmail.com>
2026-07-26 22:22:43 +0800

    Setup nativewind

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
+import "./global.css"
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className="text-3xl">Open App.js to start working on your app!</Text>
     </View>
   );
 }
-
