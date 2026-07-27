4d0f4c0e003c115be0f116b569d5e56f2631b677
SouuYe07 <iesouaguila07@gmail.com>
2026-07-27 23:22:23 +0800

    Added Background

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import "./global.css"
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "./global.css";
+import { Text, View } from 'react-native';
+import Background from "./src/components/Background";
 
 export default function App() {
   return (
-    <View>
-      <Text className="text-3xl">Open App.js to start working on your app!</Text>
-    </View>
+    <Background />
   );
 }
--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,18 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
- 
-module.exports = withNativeWind(config, { input: './global.css' })
+
+// 1. Fetch the default Expo Metro configuration
+const config = getDefaultConfig(__dirname);
+
+// 2. Extract resolver properties from the default config
+const { resolver: { sourceExts, assetExts } } = config;
+
+// 3. Configure the SVG transformer layers
+config.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer");
+
+// 4. Intercept .svg files so they are treated as source code instead of asset binaries
+config.resolver.assetExts = assetExts.filter((ext) => ext !== "svg");
+config.resolver.sourceExts = [...sourceExts, "svg"];
+
+// 5. Wrap the modified configuration with NativeWind
+module.exports = withNativeWind(config, { input: './global.css' });
